65612d1cc77c7ca74fede38f5a945845fb2f766e
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2022-03-28 13:33:22 +0200

    chore: add mvc

--- Marten5SiloedMultiTenancy/Program.cs
+++ Marten5SiloedMultiTenancy/Program.cs
@@ -2,6 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddMvc();
+
+builder.Services.AddMarten(options => options.MultiTenantedWithSingleServer("PORT = 5432; HOST = localhost; TIMEOUT = 15; POOLING = True; MINPOOLSIZE = 1; MAXPOOLSIZE = 100; COMMANDTIMEOUT = 20; DATABASE = 'marten'; PASSWORD = '123456'; USER ID = 'marten'"))
+  .ApplyAllDatabaseChangesOnStartup();
 
 var app = builder.Build();
 
@@ -20,6 +24,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapControllers();
 app.MapRazorPages();
 
 app.Run();
